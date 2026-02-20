1a65460ef5cac6fb97a0156c25add5c8e7df7dd0
Billy Vierra <bvierra@gmail.com>
2026-02-19 20:37:15 -0800

    .

--- bootstrap/talos/bin/wait-bootstrap.sh
+++ bootstrap/talos/bin/wait-bootstrap.sh
@@ -2,12 +2,19 @@
 
 while ! nc -nzv 10.10.130.101 6443 > /dev/null 2>&1; do
   echo "Waiting for Kubernetes API server to be available..."
-  sleep 1
+  sleep 10
 done
 
-task kubeconfig
+test -f ~/.kube/config && rm -f ~/.kube/config || true
+talhelper gencommand kubeconfig | bash
+
 while [ "$(kubectl get nodes --no-headers 2>/dev/null | wc -l)" -lt 1 ]; do
   echo "Waiting for at least one node to register with the cluster..."
-  sleep 1
+  sleep 10
 done
+
+# while [ "$(kubectl get nodes --no-headers 2>/dev/null | grep -c ' NotReady ')" -gt 0 ]; do
+#   echo "Waiting for all nodes to leave the NotReady state..."
+#   sleep 10
+# done
 
