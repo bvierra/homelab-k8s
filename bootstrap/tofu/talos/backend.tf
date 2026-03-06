7380170b08bc5eac98dc677f5a94c9ad0fd70adf
Billy Vierra <bvierra@gmail.com>
2026-03-05 18:19:25 -0800

    Moved to garagehq since minio is EOL

--- bootstrap/tofu/talos/backend.tf
+++ bootstrap/tofu/talos/backend.tf
@@ -1,8 +1,9 @@
 locals {
-  state_server    = "http://nas01.lab.vierra.host:9000"
-  state_bucket    = "opentofu-state"
+
+  state_insecure  = true
+  state_server    = "http://garage.lab.vierra.host:3900"
+  state_bucket    = "tofu"
   state_key       = "${var.environment}/${var.project}/opentofu.tfstate"
-  state_insecure  = false
 }
 
 terraform {
@@ -10,12 +11,10 @@ terraform {
     endpoint                    = local.state_server
     bucket                      = local.state_bucket
     key                         = local.state_key
-    region                      = "default"
-    skip_requesting_account_id  = true
+    region                      = "garage"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    skip_s3_checksum            = true
     use_path_style              = true
     insecure                    = local.state_insecure
   }
